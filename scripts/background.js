d1c3178f9de4b970f6c0dc6afced88ff10df2aea
Eurphus <p2macdon@uwaterloo.ca>
2025-01-25 15:45:25 -0500

    Removed bing opening on every reload.

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,10 @@
-console.log("OK");
+import { modifyReddit } from "./reddit";
 
 ////////////////////////////////////////////////////////////////////////////////
 // Listeners
 ////////////////////////////////////////////////////////////////////////////////
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
-    chrome.tabs.create({
-      active: true,
-      url: "https://www.bing.com/",
-    });
-  } else if (details.reason === "update") {
     chrome.tabs.create({
       active: true,
       url: "https://www.bing.com/",
